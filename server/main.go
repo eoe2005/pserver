162b334b5819cb4918215610ba5a14fc8f47dcf7
genghongfei <genghongfei@yidian-inc.com>
2022-11-17 21:33:04 +0800

    http sock5

--- server/main.go
+++ server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net"
 	"net/http"
 	"os"
 
@@ -43,6 +46,73 @@ func main() {
 		pserver.Sock5Raw(ws)
 		pserver.Debug("sock5结束")
 	})
+	r.GET("/wssock5", func(c *gin.Context) {
+		pserver.Debug("进入sock5模式")
+
+		h, ok := c.Writer.(http.Hijacker)
+		if ok {
+			con, _, e := h.Hijack()
+			if e == nil {
+				con.Write([]byte("HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: R0pRjt+Rr25+h9Irjy+Kx1ltJ0A=\r\n"))
+				helo(con)
+			} else {
+				pserver.Debug("获取链接失败 %s", e.Error())
+			}
+		} else {
+			pserver.Debug("转换失败")
+		}
+
+	})
 	pserver.Debug("服务开始运行")
 	r.Run(":" + p)
+}
+func helo(con net.Conn) {
+	h1 := make([]byte, 3)
+	con.Read(h1)
+	con.Write([]byte{0x05, 0x00})
+	h2 := make([]byte, 4)
+	con.Read(h2)
+
+	addr := ""
+	con.Write([]byte{0x05, 0x00, 0x00, h2[3]})
+	switch h2[3] {
+	case 0x01:
+		v := make([]byte, 4)
+		con.Read(v)
+		addr = fmt.Sprintf("%d.%d.%d.%d", v[0], v[1], v[2], v[3])
+		fmt.Printf("链接地址 %s\n", addr)
+		con.Write(v)
+	case 0x04:
+		v := make([]byte, 16)
+		con.Read(v)
+		con.Write(v)
+	case 0x03:
+		l := make([]byte, 1)
+		con.Read(l)
+		v := make([]byte, l[0])
+		con.Read(v)
+		con.Write(l)
+		con.Write(v)
+		addr = string(v)
+		fmt.Printf("链接地址 %s\n", addr)
+	}
+	if addr == "" {
+		con.Close()
+	}
+	p := make([]byte, 2)
+	con.Read(p)
+	port := (int(p[0]) << 8) + int(p[1])
+	fmt.Printf("链接端口 %d - %d %d %s\n", port, int(p[0])<<8, p[1], string(p))
+	desc, _ := net.Dial("tcp", fmt.Sprintf("%s:%d", addr, port))
+	con.Write(p)
+	go trance(con, desc)
+}
+
+// / 数据交换
+func trance(src, desc net.Conn) {
+	defer src.Close()
+	defer desc.Close()
+	go io.Copy(src, desc)
+	io.Copy(desc, src)
+
 }
