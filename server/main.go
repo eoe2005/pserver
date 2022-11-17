bfde984e5187f23049078d2c93a3a395ed52c5f5
eoe2005 <eoe2005@hotmail.com>
2022-11-18 00:15:26 +0800

    阿斯蒂芬

--- server/main.go
+++ server/main.go
@@ -70,27 +70,27 @@ func helo(con net.Conn) {
 	h1 := make([]byte, 3)
 	_, e := con.Read(h1)
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("1 sock init err %s", e.Error())
 		return
 	}
 
 	_, e = con.Write([]byte{0x05, 0x00})
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("2 sock init err %s", e.Error())
 		return
 	}
 
 	h2 := make([]byte, 4)
 	_, e = con.Read(h2)
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("3 sock init err %s", e.Error())
 		return
 	}
 
 	addr := ""
 	_, e = con.Write([]byte{0x05, 0x00, 0x00, h2[3]})
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("4 sock init err %s", e.Error())
 		return
 	}
 
@@ -99,7 +99,7 @@ func helo(con net.Conn) {
 		v := make([]byte, 4)
 		_, e = con.Read(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("5 sock init err %s", e.Error())
 			return
 		}
 
@@ -107,7 +107,7 @@ func helo(con net.Conn) {
 		fmt.Printf("链接地址 %s\n", addr)
 		_, e = con.Write(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("6 sock init err %s", e.Error())
 			return
 		}
 
@@ -115,38 +115,38 @@ func helo(con net.Conn) {
 		v := make([]byte, 16)
 		_, e = con.Read(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("7 sock init err %s", e.Error())
 			return
 		}
 
 		_, e = con.Write(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("8 sock init err %s", e.Error())
 			return
 		}
 	case 0x03:
 		l := make([]byte, 1)
 		_, e = con.Read(l)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("9 sock init err %s", e.Error())
 			return
 		}
 		v := make([]byte, l[0])
 		_, e = con.Read(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("10 sock init err %s", e.Error())
 			return
 		}
 
 		_, e = con.Write(l)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("11 sock init err %s", e.Error())
 			return
 		}
 
 		_, e = con.Write(v)
 		if e != nil {
-			pserver.Debug("sock init err %s", e.Error())
+			pserver.Debug("12 sock init err %s", e.Error())
 			return
 		}
 		addr = string(v)
@@ -158,7 +158,7 @@ func helo(con net.Conn) {
 	p := make([]byte, 2)
 	_, e = con.Read(p)
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("13 sock init err %s", e.Error())
 		return
 	}
 
@@ -166,13 +166,13 @@ func helo(con net.Conn) {
 	fmt.Printf("链接端口 %d - %d %d %s\n", port, int(p[0])<<8, p[1], string(p))
 	desc, e := net.Dial("tcp", fmt.Sprintf("%s:%d", addr, port))
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("14 sock init err %s", e.Error())
 		return
 	}
 
 	_, e = con.Write(p)
 	if e != nil {
-		pserver.Debug("sock init err %s", e.Error())
+		pserver.Debug("15 sock init err %s", e.Error())
 		return
 	}
 	go trance(con, desc)
@@ -180,6 +180,7 @@ func helo(con net.Conn) {
 
 // / 数据交换
 func trance(src, desc net.Conn) {
+	pserver.Debug("开始转发")
 	defer src.Close()
 	defer desc.Close()
 	go io.Copy(src, desc)
